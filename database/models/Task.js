137be33363b3322982152472f8e5f205b4dcfc84
kalanchoe18 <marcoaleman18@gmail.com>
2023-01-28 13:45:27 -0300

    Add models of DB

---
+++ database/models/Task.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    name: {
+        type : String,
+        required : true,
+        trim : true
+    },
+    description: {
+        type : String,
+        required : true,
+        trim : true,
+    },
+    dateExpire: {
+        type : Date,
+        default : Date.now()
+    },
+    state: {
+        type : Boolean,
+        default : false,
+    },
+    createBy: {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+    },
+    priority : {
+        type : String,
+        enum : ['Baja', 'Media', 'Alta'],
+        default : 'Baja'
+    },
+    project : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Project',
+    }
+}, {
+    timestamps : true
+});
+
+module.exports = mongoose.model('Task', taskSchema);
